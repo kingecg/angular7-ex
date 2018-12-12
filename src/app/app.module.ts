628411598f76ec1422278387970bf69e4a97b163
程广 <chengg@dacentsoft.com>
2018-12-12 18:18:16 +0800

    add nav on app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-
+// import {MatSidenavModule, MatIconModule,  MatToolbarModule, MatListModule} from '@angular/material'
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './mat.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
